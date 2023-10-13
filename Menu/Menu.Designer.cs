6d654410ee84c019a700d66ef57f98955ae5d33a
Somayeh SHEIKHIOSKOUEI <somayeh.sheikhioskouei@agoraplus.com>
2023-10-13 22:30:26 +0200

    Add project files.

---
+++ Menu/Menu.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Menu
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.richTxtBox = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnFolder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTxtBox
+            // 
+            this.richTxtBox.Location = new System.Drawing.Point(3, 12);
+            this.richTxtBox.Name = "richTxtBox";
+            this.richTxtBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTxtBox.Size = new System.Drawing.Size(483, 426);
+            this.richTxtBox.TabIndex = 0;
+            this.richTxtBox.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(632, 94);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(632, 134);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(632, 181);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(75, 23);
+            this.btnFont.TabIndex = 3;
+            this.btnFont.Text = "Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(632, 226);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            // 
+            // btnFolder
+            // 
+            this.btnFolder.Location = new System.Drawing.Point(632, 268);
+            this.btnFolder.Name = "btnFolder";
+            this.btnFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnFolder.TabIndex = 5;
+            this.btnFolder.Text = "Folder";
+            this.btnFolder.UseVisualStyleBackColor = true;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnFolder);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.richTxtBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formMain";
+            this.ShowInTaskbar = false;
+            this.Text = "NotdPad";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTxtBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnFolder;
+    }
+}
+
---
+++ Menu/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = InitializeOpenDialog();
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                //richTxtBox.LoadFile(openDialog.FileName);
+                richTxtBox.Text = File.ReadAllText(openDialog.FileName);
+            }
+        }
+
+        private OpenFileDialog InitializeOpenDialog()
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.Filter = "Text Files(*.txt)|*.txt|All files(*.*)|*.*";
+            openDialog.Title = "Select a file";
+            return openDialog;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = InitializeSaveDialog();
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveDialog.FileName, richTxtBox.Text);
+                //richTxtBox.SaveFile(saveDialog.FileName);
+            }
+        }
+
+        private SaveFileDialog InitializeSaveDialog()
+        {
+            var saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "Text files(*.txt)|*.txt";
+            saveDialog.Title = "Save the File";
+            saveDialog.DefaultExt = "txt";
+            return saveDialog;
+        }
+    }
+}
